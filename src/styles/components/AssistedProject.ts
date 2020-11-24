9dbec02cacd2e2fb5c172328b4b8bf7760c4222c
Fernando <fernandovalentim.cja@hotmail.com>
2020-11-24 14:02:52 -0300

    pages assistedProject

---
+++ src/styles/components/AssistedProject.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+const AssistedProject = styled.div`
+*{
+    text-align:center;
+    align-items:center;
+}
+h1{
+    color: #828282;
+    align-self:flex-start;
+    margin:10px;
+    font-family: Nunito;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 120%;
+
+}
+p{
+    color: #362A66;
+    font-family: Nunito;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 25px;
+    margin:10px
+}
+img{
+    margin:20px
+}
+`
+export default AssistedProject
